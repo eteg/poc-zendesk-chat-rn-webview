6a7d12167ec258af9d2282db24ef2b78f75ac77b
Vinicius <viniciusmarch01@gmail.com>
2023-02-14 14:34:42 -0300

    feat: code simplified

--- App.js
+++ App.js
@@ -12,36 +12,20 @@ const html = `
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Chat</title>     
-        <script>
-            window.zESettings = {
-                webWidget: {
-                    color: {
-                        theme: '#000000',
-                        launcher: '#000000', // This will also update the badge
-                        launcherText: '#000000',
-                        button: '#000000',
-                        resultLists: '#000000',
-                        header: '#000000',
-                        articleLinks: '#000000'
-                    }
-                }
-            };
-            
-        </script>
+        <title>Chat</title>    
+        <style>html {background: #071e26}</style>
         <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=${zendeskChatKey}" defer> </script>
     </head>
-    <body>        
-    </body>
+    <body></body>
 </html>
 `;
 
 const ZendeskChat = () => {
-    // const [showChat, setShowChat] = useState(false);
+    const [showChat, setShowChat] = useState(false);
 
     return (
         <View style={{ flex: 1, justifyContent: "center" }}>
-            {/* <Button title="Inicie uma conversa" color="#071e26" onPress={() => setShowChat(true)} />
+            <Button title="Inicie uma conversa" color="#071e26" onPress={() => setShowChat(true)} />
             {showChat && (
                 <Modal visible={showChat}>
                     <WebView
@@ -57,22 +41,7 @@ const ZendeskChat = () => {
                         originWhitelist={['*']}
                     />
                 </Modal>
-            )} */}
-
-                <Modal>
-                    <WebView
-                        useWebKit
-                        style={{ flex: 1 }}
-                        hideKeyboardAccessoryView
-                        source={{html, baseUrl: "https://static.zdassets.com" }}
-                        showsVerticalScrollIndicator={false}
-                        applicationNameForUserAgent="YourApp"
-                        onMessage={({ nativeEvent }) => {
-                            nativeEvent.data === "close" && setShowChat(false);
-                        }}
-                        originWhitelist={['*']}
-                    />
-                </Modal>
+            )}
         </View>
     );
 }
