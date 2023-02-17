350da29eda527b74762f12bfffc34bcbb120862e
Vinicius <viniciusmarch01@gmail.com>
2023-02-17 15:54:18 -0300

    chore: link to fiever sandbox guide

--- App.js
+++ App.js
@@ -1,47 +1,20 @@
-import React, { useState } from "react";
-import { Button, Modal, View } from "react-native";
+import React  from "react";
+import { Modal, View } from "react-native";
 import { WebView } from "react-native-webview";
-import Constants from 'expo-constants';
-
-const zendeskChatKey = Constants.expoConfig.extra.zendeskChatKey;
-
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Chat</title>    
-        <style>html {background: #071e26}</style>
-        <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=${zendeskChatKey}" defer> </script>
-    </head>
-    <body></body>
-</html>
-`;
 
 const ZendeskChat = () => {
-    const [showChat, setShowChat] = useState(false);
-
     return (
         <View style={{ flex: 1, justifyContent: "center" }}>
-            <Button title="Inicie uma conversa" color="#071e26" onPress={() => setShowChat(true)} />
-            {showChat && (
-                <Modal visible={showChat}>
-                    <WebView
-                        useWebKit
-                        style={{ flex: 1 }}
-                        hideKeyboardAccessoryView
-                        source={{html, baseUrl: "https://static.zdassets.com" }}
-                        showsVerticalScrollIndicator={false}
-                        applicationNameForUserAgent="YourApp"
-                        onMessage={({ nativeEvent }) => {
-                            nativeEvent.data === "close" && setShowChat(false);
-                        }}
-                        originWhitelist={['*']}
-                    />
-                </Modal>
-            )}
+            <Modal>
+                <WebView
+                    style={{ flex: 1 }}
+                    hideKeyboardAccessoryView
+                    source={{ uri: "https://fiever1676404393.zendesk.com/hc/pt-br" }}
+                    showsVerticalScrollIndicator={false}
+                    applicationNameForUserAgent="Fiever"
+                    originWhitelist={['*']}
+                />
+            </Modal>
         </View>
     );
 }
--- app.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'MyApp',
-    version: '1.0.0',
-    extra: {
-      zendeskChatKey: process.env.ZENDESK_CHAT_KEY,
-    },
-  };
-  
