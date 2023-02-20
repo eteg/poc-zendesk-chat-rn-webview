32477abd9972e233e0e548566537aefe031255a7
Vinicius <viniciusmarch01@gmail.com>
2023-02-20 08:51:41 -0300

    chore: adding variable isFromMobile

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React  from "react";
+import React from "react";
 import { Modal, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 const ZendeskChat = () => {
+    const INJECTED_JAVASCRIPT = `isFromMobile = true;`;
+
     return (
         <View style={{ flex: 1, justifyContent: "center" }}>
             <Modal>
@@ -11,8 +13,11 @@ const ZendeskChat = () => {
                     hideKeyboardAccessoryView
                     source={{ uri: "https://fiever.zendesk.com/hc/pt-br" }}
                     showsVerticalScrollIndicator={false}
+                    injectedJavaScript={INJECTED_JAVASCRIPT}
                     applicationNameForUserAgent="Fiever"
                     originWhitelist={['*']}
+                    cacheEnabled={false}
+                    cacheMode="LOAD_NO_CACHE"
                 />
             </Modal>
         </View>
